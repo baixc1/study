818de1f90d5baff3239840e4a1862708e1152378
baixc <863562441@qq.com>
2022-04-09 17:32:41 +0800

    链表删除

--- 前端算法与数据结构面试：底层逻辑解读与大厂真题训练/linkList.js
+++ 前端算法与数据结构面试：底层逻辑解读与大厂真题训练/linkList.js
@@ -59,6 +59,53 @@ var mergeTwoLists2 = function (l1, l2) {
   l2.next = mergeTwoLists2(l1, l2.next);
   return l2;
 };
-console.log(
-  mergeTwoLists2(generateLinkList([1, 3, 5]), generateLinkList([2, 4, 6]))
-);
+// console.log(
+//   mergeTwoLists2(generateLinkList([1, 3, 5]), generateLinkList([2, 4, 6]))
+// );
+
+/**
+ * leetcode 83. 删除排序链表中的重复元素
+ */
+var deleteDuplicates = function (head) {
+  // 将重复节点的前驱节点的next指向重复节点的next节点
+  var cur = head;
+  while (cur && cur.next) {
+    // 当前节点和下一节点重复，删除下一节点
+    if (cur.val === cur.next.val) {
+      cur.next = cur.next.next;
+    }
+    // 否则循环链表节点
+    else {
+      cur = cur.next;
+    }
+  }
+  return head;
+};
+// console.log(deleteDuplicates(generateLinkList([1, 1, 2, 22, 3, 3, 4])));
+
+/**
+ * leetcode 82. 删除排序链表中的重复元素 II
+ */
+var deleteDuplicates = function (head) {
+  // 新的头部节点（head节点可能被删除）
+  const newHead = new ListNode();
+  newHead.next = head;
+  let cur = newHead;
+  while (cur.next && cur.next.next) {
+    // 节点重复，删除重复的节点。否则继续遍历
+    if (cur.next.val === cur.next.next.val) {
+      // 被删除节点值
+      const val = cur.next.val;
+      // 删除两个重复节点
+      cur.next = cur.next.next.next;
+      // 继续删除重复节点
+      while (cur.next && cur.next.val === val) {
+        cur.next = cur.next.next;
+      }
+    } else {
+      cur = cur.next;
+    }
+  }
+  return newHead.next;
+};
+console.log(deleteDuplicates(generateLinkList([1, 1, 1, 2, 2, 3, 4, 5])));
