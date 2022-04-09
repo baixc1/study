9cd6991f302562a61dcd8780ba9f472ebbcae040
baixc <863562441@qq.com>
2022-04-09 16:32:21 +0800

    链表合并

--- 前端算法与数据结构面试：底层逻辑解读与大厂真题训练/linkList.js
+++ 前端算法与数据结构面试：底层逻辑解读与大厂真题训练/linkList.js
@@ -30,24 +30,35 @@ function generateLinkList(list) {
 var mergeTwoLists = function (l1, l2) {
   // 头节点
   let head = new ListNode();
-  // 编译两个链表
+  // 指针
+  let cur = head;
+  // 遍历两个链表
   while (l1 && l2) {
     if (l1.val < l2.val) {
-      head.next = l1;
+      cur.next = l1;
       l1 = l1.next;
     } else {
-      head.next = l2;
+      cur.next = l2;
       l2 = l2.next;
     }
+    // 指针前移
+    cur = cur.next;
   }
   // 两个链表未遍历部分
-  if (l1) {
-    l2.next = l1;
-  } else if (l2) {
-    l1.next = l2;
-  }
+  cur.next = l1 ? l1 : l2;
   return head.next;
 };
+// 递归
+var mergeTwoLists2 = function (l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists2(l1.next, l2);
+    return l1;
+  }
+  l2.next = mergeTwoLists2(l1, l2.next);
+  return l2;
+};
 console.log(
-  mergeTwoLists(generateLinkList([1, 3, 5]), generateLinkList([2, 4, 6]))
+  mergeTwoLists2(generateLinkList([1, 3, 5]), generateLinkList([2, 4, 6]))
 );
