7eaa5e446bb2c1fbbe325dea61549e1d2add3cf2
baixc <863562441@qq.com>
2022-04-25 15:47:14 +0800

    数组子集递归

--- 前端算法与数据结构面试：底层逻辑解读与大厂真题训练/DFSAndBFS/index.md
+++ 前端算法与数据结构面试：底层逻辑解读与大厂真题训练/DFSAndBFS/index.md
@@ -4,3 +4,24 @@
 - 关键词：穷举，重复 => 递归函数 => DFS
 - 递归函数：第一次在 n 个数中选一个（循环），第二次在 n-1 个数中选一个, ...在 1 个数中选一个（树形结构）（标记已使用元素）
 - 终止条件：先序遍历，遍历到的元素加入 cur 数组，cur 长度等于原数组时终止。返回时 cur 弹出该元素，并标记为未使用。(场景回溯)
+
+### 数组子集
+
+- subsetsWithDup.js
+- 递归函数+终止条件
+
+```javascript
+var subsetsWithDup = function (nums) {
+  const len = nums.length;
+  const ret = []; // 所有结果
+  const sub = []; // 一次结果
+  dfs(0);
+  // 求数组子集的递归函数
+  function dfs(index) {
+    for (let i = index; i < len; i++) {
+      dfs(i + 1);
+    }
+  }
+  return ret;
+};
+```
--- 前端算法与数据结构面试：底层逻辑解读与大厂真题训练/DFSAndBFS/subsetsWithDup.js
+++ 前端算法与数据结构面试：底层逻辑解读与大厂真题训练/DFSAndBFS/subsetsWithDup.js
@@ -3,4 +3,28 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var subsetsWithDup = function (nums) {};
+var subsetsWithDup = function (nums) {
+  // 初始化变量
+  const len = nums.length;
+  const ret = []; // 所有结果
+  const sub = []; // 一次结果
+  const dict = {};
+  nums.sort((a, b) => a - b);
+  dfs(0);
+  // 递归函数
+  function dfs(index) {
+    const key = sub.join();
+    if (!dict[key]) {
+      ret.push(sub.slice());
+      dict[key] = 1;
+    }
+
+    for (let i = index; i < len; i++) {
+      sub.push(nums[i]);
+      dfs(i + 1);
+      sub.pop();
+    }
+  }
+  return ret;
+};
+console.log(subsetsWithDup([4, 4, 4, 1, 4]));
